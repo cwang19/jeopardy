c407f73cb813b07c14d8a65afc74f9b4e2b94dbf
Cassie Wang <cwang265@gmail.com>
2019-11-02 17:14:48 -0400

    Updates

---
+++ precache-manifest.aebd3fa202c51a21544838b1b21550e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761dec9679a5e4c7e4a2cbd61a9313d1",
+    "url": "/jeopardy/index.html"
+  },
+  {
+    "revision": "db1ea3e2af1f15c84ae3",
+    "url": "/jeopardy/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "482af60e13d144a0ee3f",
+    "url": "/jeopardy/static/js/2.edc27215.chunk.js"
+  },
+  {
+    "revision": "db1ea3e2af1f15c84ae3",
+    "url": "/jeopardy/static/js/main.392e6868.chunk.js"
+  },
+  {
+    "revision": "5543fe1ddd9bf3dc332c",
+    "url": "/jeopardy/static/js/runtime-main.f5a61539.js"
+  }
+]);
